f70feb49085b92546f7d1c47d5829b60fd2d3f36
Gilles <g.biascamano.ext@fayatit.fayat.com>
2026-03-12 14:42:13 +0100

    Configuration via Feature Flag (MaintenanceMode) test v1

--- AzureQuizLab/Pages/Index.cshtml.cs
+++ AzureQuizLab/Pages/Index.cshtml.cs
@@ -6,15 +6,19 @@ namespace AzureQuizLab.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public bool MaintenanceMode { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            MaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode", false);
         }
     }
 }
